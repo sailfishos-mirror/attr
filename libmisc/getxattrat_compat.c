sailfishos-mirror/acl

Add xattrat syscall backwards compatibility code
The getxattrat(), setxattrat(), listxattrat(), and removexattrat() system calls
have only been added in kernel version 6.13 which was released in January 2025.
On older systems, those system calls will fail with errno == ENOSYS.  So we use
the old system calls (for example, getxattr() and lgetxattr()) when they are
sufficient, and the new xattrat() system calls when they are needed.

In addition, when the new system calls are not available, we resort to the
following hacks for achieving the desired behavior: when dirfd is not AT_FDCWD,
the pathname can usually be resolved relative to dirfd as
"/proc/self/fd/<dirfd>/pathname".  And when that also fails because the
resulting pathname is too long, the original pathname can be resolved using
openat(dirfd, pathname, O_PATH) and then accessed as "/proc/self/fd/<fd>".

Also, due to a current kernel limitation, the xattrat system calls will not
accept dirfd file descriptors of type O_PATH with an empty pathname (and with
the AT_EMPTY_PATH flag set in at_flags) [*].  When running into this case, we
also fall back to accessing the file via "/proc/self/fd/<fd>".

When those hacks fail because the /proc filesystem is not available, these
compatibility wrappers will fail with errno == ENOSYS.

[*] https://lore.kernel.org/linux-fsdevel/20260607164136.2948550-2-agruenba@redhat.com/

Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>

libmisc/getxattrat_compat.c
@@ -1,0 +1,108 @@
+/*
+  File: getxattrat_compat.c
+
+  Copyright (C) 2026 Andreas Gruenbacher <andreas.gruenbacher@gmail.com>
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "config.h"
+#include <sys/xattr.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include "xattrat.h"
+#include "libmisc/proc-self-fd.h"
+
+#define DECLARATIONS_ONLY
+#include "xattrat_compat.h"
+
+/* Used in {set,list,remove}xattrat_compat.c */
+int slow_empty_paths;
+
+static ssize_t
+xgetxattr(const char *path, int at_flags, const char *name, void *value,
+	  size_t size)
+{
+	if (at_flags & AT_SYMLINK_NOFOLLOW)
+		return lgetxattr(path, name, value, size);
+	return getxattr(path, name, value, size);
+}
+
+ssize_t
+getxattrat_compat(int dirfd, const char *path, int at_flags, const char *name,
+		  void *value, size_t size)
+{
+	struct proc_self_fd_buffer buffer;
+	static int no_getxattrat;
+	char *newpath;
+	ssize_t ret;
+	int fd;
+
+	if (at_flags & ~(AT_EMPTY_PATH | AT_SYMLINK_NOFOLLOW)) {
+		errno = EINVAL;
+		return -1;
+	}
+	if (!*path) {
+		if (!(at_flags & AT_EMPTY_PATH)) {
+			errno = ENOENT;
+			return -1;
+		}
+	}
+
+	if (dirfd == AT_FDCWD || *path == '/')
+		return xgetxattr(path, at_flags, name, value, size);
+
+	if (!no_getxattrat) {
+		if (!*path && slow_empty_paths)
+			goto proc_self_fd_path;
+		ret = getxattrat(dirfd, path, at_flags, name, value, size);
+		if (ret != -1)
+		       return ret;
+		if (need_slow_empty_paths(dirfd, path)) {
+			slow_empty_paths = 1;
+			goto proc_self_fd_path;
+		}
+		if (errno != ENOSYS)
+			return -1;
+		no_getxattrat = 1;
+	}
+
+proc_self_fd_path:
+	newpath = proc_self_fd_realpath(dirfd, path, &at_flags, &buffer);
+	if (!newpath)
+		return -1;
+	ret = xgetxattr(newpath, at_flags, name, value, size);
+	proc_self_fd_free(newpath, &buffer);
+	if (ret != -1)
+		return ret;
+	if (errno == ENOENT) {
+		if (!*path) {
+			errno = EBADF;
+			return -1;
+		}
+		if (fcntl(dirfd, F_GETFL) != -1)
+			errno = ENOENT;
+		return -1;
+	} else if (errno != ENAMETOOLONG)
+		return ret;
+
+	fd = openat_with_flags(dirfd, path, at_flags);
+	if (fd == -1)
+		return -1;
+	ret = getxattr(proc_self_fd_path(fd, &buffer), name, value, size);
+	close(fd);
+	return ret;
+}

libmisc/listxattrat_compat.c
@@ -1,0 +1,107 @@
+/*
+  File: listxattrat_compat.c
+
+  Copyright (C) 2026 Andreas Gruenbacher <andreas.gruenbacher@gmail.com>
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "config.h"
+#include <sys/xattr.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include "xattrat.h"
+#include "libmisc/proc-self-fd.h"
+
+#define DECLARATIONS_ONLY
+#include "xattrat_compat.h"
+
+/* Defined in getxattrat_compat.c */
+extern int slow_empty_paths;
+
+static ssize_t
+xlistxattr(const char *path, int at_flags, char *list, size_t size)
+{
+	if (at_flags & AT_SYMLINK_NOFOLLOW)
+		return llistxattr(path, list, size);
+	return listxattr(path, list, size);
+}
+
+ssize_t
+listxattrat_compat(int dirfd, const char *path, int at_flags, char *list,
+		   size_t size)
+{
+	struct proc_self_fd_buffer buffer;
+	static int no_listxattrat;
+	char *newpath;
+	ssize_t ret;
+	int fd;
+
+	if (at_flags & ~(AT_EMPTY_PATH | AT_SYMLINK_NOFOLLOW)) {
+		errno = EINVAL;
+		return -1;
+	}
+	if (!*path) {
+		if (!(at_flags & AT_EMPTY_PATH)) {
+			errno = ENOENT;
+			return -1;
+		}
+	}
+
+	if (dirfd == AT_FDCWD || *path == '/')
+		return xlistxattr(path, at_flags, list, size);
+
+	if (!no_listxattrat) {
+		if (!*path && slow_empty_paths)
+			goto proc_self_fd_path;
+		ret = listxattrat(dirfd, path, at_flags, list, size);
+		if (ret != -1)
+		       return ret;
+		if (need_slow_empty_paths(dirfd, path)) {
+			slow_empty_paths = 1;
+			goto proc_self_fd_path;
+		}
+		if (errno != ENOSYS)
+			return -1;
+		no_listxattrat = 1;
+	}
+
+proc_self_fd_path:
+	newpath = proc_self_fd_realpath(dirfd, path, &at_flags, &buffer);
+	if (!newpath)
+		return -1;
+	ret = xlistxattr(newpath, at_flags, list, size);
+	proc_self_fd_free(newpath, &buffer);
+	if (ret != -1)
+		return ret;
+	if (errno == ENOENT) {
+		if (!*path) {
+			errno = EBADF;
+			return -1;
+		}
+		if (fcntl(dirfd, F_GETFL) != -1)
+			errno = ENOENT;
+		return -1;
+	} else if (errno != ENAMETOOLONG)
+		return ret;
+
+	fd = openat_with_flags(dirfd, path, at_flags);
+	if (fd == -1)
+		return -1;
+	ret = listxattr(proc_self_fd_path(fd, &buffer), list, size);
+	close(fd);
+	return ret;
+}

libmisc/removexattrat_compat.c
@@ -1,0 +1,107 @@
+/*
+  File: getxattrat_compat.c
+
+  Copyright (C) 2026 Andreas Gruenbacher <andreas.gruenbacher@gmail.com>
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "config.h"
+#include <sys/xattr.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include "xattrat.h"
+#include "libmisc/proc-self-fd.h"
+
+#define DECLARATIONS_ONLY
+#include "xattrat_compat.h"
+
+/* Defined in getxattrat_compat.c */
+extern int slow_empty_paths;
+
+static int
+xremovexattr(const char *path, int at_flags, const char *name)
+{
+	if (at_flags & AT_SYMLINK_NOFOLLOW)
+		return lremovexattr(path, name);
+	return removexattr(path, name);
+}
+
+
+int
+removexattrat_compat(int dirfd, const char *path, int at_flags,
+		     const char *name)
+{
+	struct proc_self_fd_buffer buffer;
+	static int no_removexattrat;
+	char *newpath;
+	int fd, ret;
+
+	if (at_flags & ~(AT_EMPTY_PATH | AT_SYMLINK_NOFOLLOW)) {
+		errno = EINVAL;
+		return -1;
+	}
+	if (!*path) {
+		if (!(at_flags & AT_EMPTY_PATH)) {
+			errno = ENOENT;
+			return -1;
+		}
+	}
+
+	if (dirfd == AT_FDCWD || *path == '/')
+		return xremovexattr(path, at_flags, name);
+
+	if (!no_removexattrat) {
+		if (!*path && slow_empty_paths)
+			goto proc_self_fd_path;
+		ret = removexattrat(dirfd, path, at_flags, name);
+		if (ret != -1)
+			return ret;
+		if (need_slow_empty_paths(dirfd, path)) {
+			slow_empty_paths = 1;
+			goto proc_self_fd_path;
+		}
+		if (errno != ENOSYS)
+			return ret;
+		no_removexattrat = 1;
+	}
+
+proc_self_fd_path:
+	newpath = proc_self_fd_realpath(dirfd, path, &at_flags, &buffer);
+	if (!newpath)
+		return -1;
+	ret = xremovexattr(newpath, at_flags, name);
+	proc_self_fd_free(newpath, &buffer);
+	if (ret != -1)
+		return ret;
+	if (errno == ENOENT) {
+		if (!*path) {
+			errno = EBADF;
+			return -1;
+		}
+		if (fcntl(dirfd, F_GETFL) != -1)
+			errno = ENOENT;
+		return -1;
+	} else if (errno != ENAMETOOLONG)
+		return ret;
+
+	fd = openat_with_flags(dirfd, path, at_flags);
+	if (fd == -1)
+		return -1;
+	ret = removexattr(proc_self_fd_path(fd, &buffer), name);
+	close(fd);
+	return ret;
+}

libmisc/setxattrat_compat.c
@@ -1,0 +1,108 @@
+/*
+  File: getxattrat_compat.c
+
+  Copyright (C) 2026 Andreas Gruenbacher <andreas.gruenbacher@gmail.com>
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "config.h"
+#include <sys/xattr.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include "xattrat.h"
+#include "libmisc/proc-self-fd.h"
+
+#define DECLARATIONS_ONLY
+#include "xattrat_compat.h"
+
+/* Defined in getxattrat_compat.c */
+extern int slow_empty_paths;
+
+static int
+xsetxattr(const char *path, int at_flags, const char *name, const void *value,
+	  size_t size, int flags)
+{
+	if (at_flags & AT_SYMLINK_NOFOLLOW)
+		return lsetxattr(path, name, value, size, flags);
+	return setxattr(path, name, value, size, flags);
+}
+
+int
+setxattrat_compat(int dirfd, const char *path, int at_flags, const char *name,
+		  const void *value, size_t size, int flags)
+{
+	struct proc_self_fd_buffer buffer;
+	static int no_setxattrat;
+	char *newpath;
+	int fd, ret;
+
+	if (at_flags & ~(AT_EMPTY_PATH | AT_SYMLINK_NOFOLLOW)) {
+		errno = EINVAL;
+		return -1;
+	}
+	if (!*path) {
+		if (!(at_flags & AT_EMPTY_PATH)) {
+			errno = ENOENT;
+			return -1;
+		}
+	}
+
+	if (dirfd == AT_FDCWD || *path == '/')
+		return xsetxattr(path, at_flags, name, value, size, flags);
+
+	if (!no_setxattrat) {
+		if (!*path && slow_empty_paths)
+			goto proc_self_fd_path;
+		ret = setxattrat(dirfd, path, at_flags, name, value, size,
+				 flags);
+		if (ret != -1)
+			return ret;
+		if (need_slow_empty_paths(dirfd, path)) {
+			slow_empty_paths = 1;
+			goto proc_self_fd_path;
+		}
+		if (errno != ENOSYS)
+			return ret;
+		no_setxattrat = 1;
+	}
+
+proc_self_fd_path:
+	newpath = proc_self_fd_realpath(dirfd, path, &at_flags, &buffer);
+	if (!newpath)
+		return -1;
+	ret = xsetxattr(newpath, at_flags, name, value, size, flags);
+	proc_self_fd_free(newpath, &buffer);
+	if (ret != -1)
+		return ret;
+	if (errno == ENOENT) {
+		if (!*path) {
+			errno = EBADF;
+			return -1;
+		}
+		if (fcntl(dirfd, F_GETFL) != -1)
+			errno = ENOENT;
+		return -1;
+	} else if (errno != ENAMETOOLONG)
+		return ret;
+
+	fd = openat_with_flags(dirfd, path, at_flags);
+	if (fd == -1)
+		return -1;
+	ret = setxattr(proc_self_fd_path(fd, &buffer), name, value, size, flags);
+	close(fd);
+	return ret;
+}

sailfishos-mirror/attr

libmisc/getxattrat_compat.c
@@ -1,0 +1,108 @@
+/*
+  File: getxattrat_compat.c
+
+  Copyright (C) 2026 Andreas Gruenbacher <andreas.gruenbacher@gmail.com>
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "config.h"
+#include <sys/xattr.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include "xattrat.h"
+#include "libmisc/proc-self-fd.h"
+
+#define DECLARATIONS_ONLY
+#include "xattrat_compat.h"
+
+/* Used in {set,list,remove}xattrat_compat.c */
+int slow_empty_paths;
+
+static ssize_t
+xgetxattr(const char *path, int at_flags, const char *name, void *value,
+	  size_t size)
+{
+	if (at_flags & AT_SYMLINK_NOFOLLOW)
+		return lgetxattr(path, name, value, size);
+	return getxattr(path, name, value, size);
+}
+
+ssize_t
+getxattrat_compat(int dirfd, const char *path, int at_flags, const char *name,
+		  void *value, size_t size)
+{
+	struct proc_self_fd_buffer buffer;
+	static int no_getxattrat;
+	char *newpath;
+	ssize_t ret;
+	int fd;
+
+	if (at_flags & ~(AT_EMPTY_PATH | AT_SYMLINK_NOFOLLOW)) {
+		errno = EINVAL;
+		return -1;
+	}
+	if (!*path) {
+		if (!(at_flags & AT_EMPTY_PATH)) {
+			errno = ENOENT;
+			return -1;
+		}
+	}
+
+	if (dirfd == AT_FDCWD || *path == '/')
+		return xgetxattr(path, at_flags, name, value, size);
+
+	if (!no_getxattrat) {
+		if (!*path && slow_empty_paths)
+			goto proc_self_fd_path;
+		ret = getxattrat(dirfd, path, at_flags, name, value, size);
+		if (ret != -1)
+		       return ret;
+		if (need_slow_empty_paths(dirfd, path)) {
+			slow_empty_paths = 1;
+			goto proc_self_fd_path;
+		}
+		if (errno != ENOSYS)
+			return -1;
+		no_getxattrat = 1;
+	}
+
+proc_self_fd_path:
+	newpath = proc_self_fd_realpath(dirfd, path, &at_flags, &buffer);
+	if (!newpath)
+		return -1;
+	ret = xgetxattr(newpath, at_flags, name, value, size);
+	proc_self_fd_free(newpath, &buffer);
+	if (ret != -1)
+		return ret;
+	if (errno == ENOENT) {
+		if (!*path) {
+			errno = ENOSYS;
+			return -1;
+		}
+		if (fcntl(dirfd, F_GETFL) != -1)
+			errno = ENOENT;
+		return -1;
+	} else if (errno != ENAMETOOLONG)
+		return ret;
+
+	fd = openat_with_flags(dirfd, path, at_flags);
+	if (fd == -1)
+		return -1;
+	ret = getxattr(proc_self_fd_path(fd, &buffer), name, value, size);
+	close(fd);
+	return ret;
+}

libmisc/listxattrat_compat.c
@@ -1,0 +1,107 @@
+/*
+  File: listxattrat_compat.c
+
+  Copyright (C) 2026 Andreas Gruenbacher <andreas.gruenbacher@gmail.com>
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "config.h"
+#include <sys/xattr.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include "xattrat.h"
+#include "libmisc/proc-self-fd.h"
+
+#define DECLARATIONS_ONLY
+#include "xattrat_compat.h"
+
+/* Defined in getxattrat_compat.c */
+extern int slow_empty_paths;
+
+static ssize_t
+xlistxattr(const char *path, int at_flags, char *list, size_t size)
+{
+	if (at_flags & AT_SYMLINK_NOFOLLOW)
+		return llistxattr(path, list, size);
+	return listxattr(path, list, size);
+}
+
+ssize_t
+listxattrat_compat(int dirfd, const char *path, int at_flags, char *list,
+		   size_t size)
+{
+	struct proc_self_fd_buffer buffer;
+	static int no_listxattrat;
+	char *newpath;
+	ssize_t ret;
+	int fd;
+
+	if (at_flags & ~(AT_EMPTY_PATH | AT_SYMLINK_NOFOLLOW)) {
+		errno = EINVAL;
+		return -1;
+	}
+	if (!*path) {
+		if (!(at_flags & AT_EMPTY_PATH)) {
+			errno = ENOENT;
+			return -1;
+		}
+	}
+
+	if (dirfd == AT_FDCWD || *path == '/')
+		return xlistxattr(path, at_flags, list, size);
+
+	if (!no_listxattrat) {
+		if (!*path && slow_empty_paths)
+			goto proc_self_fd_path;
+		ret = listxattrat(dirfd, path, at_flags, list, size);
+		if (ret != -1)
+		       return ret;
+		if (need_slow_empty_paths(dirfd, path)) {
+			slow_empty_paths = 1;
+			goto proc_self_fd_path;
+		}
+		if (errno != ENOSYS)
+			return -1;
+		no_listxattrat = 1;
+	}
+
+proc_self_fd_path:
+	newpath = proc_self_fd_realpath(dirfd, path, &at_flags, &buffer);
+	if (!newpath)
+		return -1;
+	ret = xlistxattr(newpath, at_flags, list, size);
+	proc_self_fd_free(newpath, &buffer);
+	if (ret != -1)
+		return ret;
+	if (errno == ENOENT) {
+		if (!*path) {
+			errno = ENOSYS;
+			return -1;
+		}
+		if (fcntl(dirfd, F_GETFL) != -1)
+			errno = ENOENT;
+		return -1;
+	} else if (errno != ENAMETOOLONG)
+		return ret;
+
+	fd = openat_with_flags(dirfd, path, at_flags);
+	if (fd == -1)
+		return -1;
+	ret = listxattr(proc_self_fd_path(fd, &buffer), list, size);
+	close(fd);
+	return ret;
+}

libmisc/removexattrat_compat.c
@@ -1,0 +1,107 @@
+/*
+  File: getxattrat_compat.c
+
+  Copyright (C) 2026 Andreas Gruenbacher <andreas.gruenbacher@gmail.com>
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "config.h"
+#include <sys/xattr.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include "xattrat.h"
+#include "libmisc/proc-self-fd.h"
+
+#define DECLARATIONS_ONLY
+#include "xattrat_compat.h"
+
+/* Defined in getxattrat_compat.c */
+extern int slow_empty_paths;
+
+static int
+xremovexattr(const char *path, int at_flags, const char *name)
+{
+	if (at_flags & AT_SYMLINK_NOFOLLOW)
+		return lremovexattr(path, name);
+	return removexattr(path, name);
+}
+
+
+int
+removexattrat_compat(int dirfd, const char *path, int at_flags,
+		     const char *name)
+{
+	struct proc_self_fd_buffer buffer;
+	static int no_removexattrat;
+	char *newpath;
+	int fd, ret;
+
+	if (at_flags & ~(AT_EMPTY_PATH | AT_SYMLINK_NOFOLLOW)) {
+		errno = EINVAL;
+		return -1;
+	}
+	if (!*path) {
+		if (!(at_flags & AT_EMPTY_PATH)) {
+			errno = ENOENT;
+			return -1;
+		}
+	}
+
+	if (dirfd == AT_FDCWD || *path == '/')
+		return xremovexattr(path, at_flags, name);
+
+	if (!no_removexattrat) {
+		if (!*path && slow_empty_paths)
+			goto proc_self_fd_path;
+		ret = removexattrat(dirfd, path, at_flags, name);
+		if (ret != -1)
+			return ret;
+		if (need_slow_empty_paths(dirfd, path)) {
+			slow_empty_paths = 1;
+			goto proc_self_fd_path;
+		}
+		if (errno != ENOSYS)
+			return ret;
+		no_removexattrat = 1;
+	}
+
+proc_self_fd_path:
+	newpath = proc_self_fd_realpath(dirfd, path, &at_flags, &buffer);
+	if (!newpath)
+		return -1;
+	ret = xremovexattr(newpath, at_flags, name);
+	proc_self_fd_free(newpath, &buffer);
+	if (ret != -1)
+		return ret;
+	if (errno == ENOENT) {
+		if (!*path) {
+			errno = ENOSYS;
+			return -1;
+		}
+		if (fcntl(dirfd, F_GETFL) != -1)
+			errno = ENOENT;
+		return -1;
+	} else if (errno != ENAMETOOLONG)
+		return ret;
+
+	fd = openat_with_flags(dirfd, path, at_flags);
+	if (fd == -1)
+		return -1;
+	ret = removexattr(proc_self_fd_path(fd, &buffer), name);
+	close(fd);
+	return ret;
+}

libmisc/setxattrat_compat.c
@@ -1,0 +1,108 @@
+/*
+  File: getxattrat_compat.c
+
+  Copyright (C) 2026 Andreas Gruenbacher <andreas.gruenbacher@gmail.com>
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "config.h"
+#include <sys/xattr.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include "xattrat.h"
+#include "libmisc/proc-self-fd.h"
+
+#define DECLARATIONS_ONLY
+#include "xattrat_compat.h"
+
+/* Defined in getxattrat_compat.c */
+extern int slow_empty_paths;
+
+static int
+xsetxattr(const char *path, int at_flags, const char *name, const void *value,
+	  size_t size, int flags)
+{
+	if (at_flags & AT_SYMLINK_NOFOLLOW)
+		return lsetxattr(path, name, value, size, flags);
+	return setxattr(path, name, value, size, flags);
+}
+
+int
+setxattrat_compat(int dirfd, const char *path, int at_flags, const char *name,
+		  const void *value, size_t size, int flags)
+{
+	struct proc_self_fd_buffer buffer;
+	static int no_setxattrat;
+	char *newpath;
+	int fd, ret;
+
+	if (at_flags & ~(AT_EMPTY_PATH | AT_SYMLINK_NOFOLLOW)) {
+		errno = EINVAL;
+		return -1;
+	}
+	if (!*path) {
+		if (!(at_flags & AT_EMPTY_PATH)) {
+			errno = ENOENT;
+			return -1;
+		}
+	}
+
+	if (dirfd == AT_FDCWD || *path == '/')
+		return xsetxattr(path, at_flags, name, value, size, flags);
+
+	if (!no_setxattrat) {
+		if (!*path && slow_empty_paths)
+			goto proc_self_fd_path;
+		ret = setxattrat(dirfd, path, at_flags, name, value, size,
+				 flags);
+		if (ret != -1)
+			return ret;
+		if (need_slow_empty_paths(dirfd, path)) {
+			slow_empty_paths = 1;
+			goto proc_self_fd_path;
+		}
+		if (errno != ENOSYS)
+			return ret;
+		no_setxattrat = 1;
+	}
+
+proc_self_fd_path:
+	newpath = proc_self_fd_realpath(dirfd, path, &at_flags, &buffer);
+	if (!newpath)
+		return -1;
+	ret = xsetxattr(newpath, at_flags, name, value, size, flags);
+	proc_self_fd_free(newpath, &buffer);
+	if (ret != -1)
+		return ret;
+	if (errno == ENOENT) {
+		if (!*path) {
+			errno = ENOSYS;
+			return -1;
+		}
+		if (fcntl(dirfd, F_GETFL) != -1)
+			errno = ENOENT;
+		return -1;
+	} else if (errno != ENAMETOOLONG)
+		return ret;
+
+	fd = openat_with_flags(dirfd, path, at_flags);
+	if (fd == -1)
+		return -1;
+	ret = setxattr(proc_self_fd_path(fd, &buffer), name, value, size, flags);
+	close(fd);
+	return ret;
+}